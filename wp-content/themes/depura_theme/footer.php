6868ee06764ccc9f5fc58b14e0a492fc3df0b541
Camilo <kmilo.vanegas@gmail.com>
2020-10-07 06:17:27 -0500

    Fix index.php instance

--- wp-content/themes/depura_theme/footer.php
+++ wp-content/themes/depura_theme/footer.php
@@ -4,32 +4,11 @@
  *
  * Contains the closing of the #content div and all content after
  *
- * @package storefront
+ * @package depura
  */
 
 ?>
 
-		</div><!-- .col-full -->
-	</div><!-- #content -->
-
-	<?php do_action( 'storefront_before_footer' ); ?>
-
-	<footer id="colophon" class="site-footer footer" role="contentinfo">
-		<div class="footer-row container">
-			<?php dynamic_sidebar('footer-widget-area'); ?>
-		</div>
-		<div class="footer-copyright">
-			<div class="footer-row container">
-				<div class="footer-copyright__text">
-					<p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
-				</div>
-			</div>
-		</div>
-	</footer><!-- #colophon -->
-
-	<?php do_action( 'storefront_after_footer' ); ?>
-
-</div><!-- #page -->
 
 <?php wp_footer(); ?>
 
